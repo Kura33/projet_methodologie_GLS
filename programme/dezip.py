ca21f0c49ad1861306b27c17c03d8b31e0e1c2ab
Clément MARCEL <35737100+lecramc@users.noreply.github.com>
2020-05-14 11:54:54 +0200

    Delete dezip.py

--- programme/dezip.py
+++
@@ -1,9 +0,0 @@
-from zipfile import ZipFile
-
-def dezip(fileName):
-
-    file = fileName
-
-    with ZipFile(file, 'r') as zip: 
-        # extraire tous les fichiers vers un autre répertoire
-        zip.extractall('data')
